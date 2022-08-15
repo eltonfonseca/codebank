800ab55d9187aaa6da3c5299ab4f5616a616f43c
Elton Fonseca <eltonjuniorfonseca@gmail.com>
2022-08-15 20:41:21 -0300

    feat: add credit card domain

---
+++ domains/credit_card.go
@@ -0,0 +1,27 @@
+package domains
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type CreditCard struct {
+	ID              uuid.UUID
+	Name            string
+	Number          string
+	ExpirationMonth int32
+	ExpirationYear  int32
+	CVV             int32
+	Balance         float64
+	Limit           float64
+	CreatedAt       time.Time
+}
+
+func NewCreditCard() CreditCard {
+	c := CreditCard{}
+	c.ID = uuid.New()
+	c.CreatedAt = time.Now()
+
+	return c
+}
